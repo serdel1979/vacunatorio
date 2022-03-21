e52e3311e781e0c97ced19e8451acb742caae878
Sergio De Luca <sdlbsso@gmail.com>
2022-03-21 09:56:34 -0300

    haciendo login

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
+from wsgiref.validate import validator
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from sqlalchemy import true
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, DateField
+from wtforms.validators import InputRequired, Email, Length
+
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='Est4Cl4v3e5Sup3rS3cr3t4'
+
 Bootstrap(app)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Usuario',validators=[InputRequired(),Length(min=4,max=15)])
+    password = PasswordField('Contraseña',validators=[InputRequired(),Length(min=4,max=15)])
+
+class RegistroForm(FlaskForm):
+    usuario = StringField('Usuario',validators=[InputRequired(),Length(min=4,max=20)])
+    nombre = StringField('Nombre',validators=[InputRequired(),Length(min=4,max=20)])
+    apellido = StringField('Apellido',validators=[InputRequired(),Length(min=4,max=20)])
+    telefono = StringField('Teléfono',validators=[InputRequired(),Length(min=4,max=20)])
+    nacimiento = DateField('Fecha de nacimiento')
+    primera_dosis = BooleanField('Tengo la primera dósis')
+    paciente_riesgo = BooleanField('Soy paciente de riesgo')
+    password = PasswordField('Escriba una contraseña',validators=[InputRequired(),Length(min=4,max=15)])
+    email = StringField('Email', validators=[InputRequired(), Email(message='Email inválido'), Length(max=50)])
+    dni = StringField('Dni',validators=[InputRequired(),Length(min=4,max=15)])
+
+
 
 @app.route('/')
 def index():
@@ -13,12 +38,14 @@ def index():
 
 @app.route('/login')
 def login():
-    return render_template('index.html') 
+    form = LoginForm()
+    return render_template('login.html',form=form) 
 
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html') 
+@app.route('/registro')
+def registro():
+    form = RegistroForm()
+    return render_template('registro.html',form=form) 
 
 
 @app.route('/dashboard')
@@ -27,4 +54,4 @@ def dashboard():
 
 
 if __name__ == '__main__':
-    app.run(debug=true)
+    app.run(debug=True)

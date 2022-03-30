5093b425397889bcdd23682c892a609347905efc
Sergio De Luca <sdlbsso@gmail.com>
2022-03-29 23:13:55 -0300

    agregado check de fiebre amarilla en el registro

--- app/model/user.py
+++ app/model/user.py
@@ -14,6 +14,7 @@ class User(db.Model, UserMixin):
     nacimiento = db.Column(db.String(20))
     primera_dosis = db.Column(db.String(20))
     paciente_riesgo = db.Column(db.String(20))
+    fiebre_amarilla = db.Column(db.String(20))
     password = db.Column(db.String(20))
     email = db.Column(db.String(20))
     dni = db.Column(db.String(20))
@@ -21,13 +22,14 @@ class User(db.Model, UserMixin):
     tipo = db.Column(db.Integer)
     sede = db.Column(db.String(20))
 
-    def __init__(self,usuario,nombre,apellido,telefono=None,nacimiento=None,primera_dosis=None,paciente_riesgo=None,password=None,email=None,dni=None,tipo=3, sede_preferida=None,sede=None):
+    def __init__(self,usuario,nombre,apellido,telefono=None,nacimiento=None,primera_dosis=None,paciente_riesgo=None,fiebre_amarilla=None,password=None,email=None,dni=None,tipo=3, sede_preferida=None,sede=None):
         self.usuario = usuario
         self.nombre = nombre
         self.apellido = apellido
         self.telefono =telefono
         self.nacimiento = nacimiento
         self.primera_dosis = primera_dosis
+        self.fiebre_amarilla = fiebre_amarilla
         self.paciente_riesgo = paciente_riesgo
         self.password = password
         self.email = email

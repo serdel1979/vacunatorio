96360ea8d8d0f063bd27cf7506f7c9fa8f210ba5
Alejandro Antelo <alejandronicolasantelo@gmail.com>
2022-04-30 15:46:37 -0300

    modificacion de pw bd

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__,template_folder='templates')
-    app.config['SECRET_KEY']='B!1weNAt1T^%kvhUI*S^'
-    app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@127.0.0.1:3306/bdvacunatorio'
+    app.config['SECRET_KEY']='B!1weNAt1T^%'
+    app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:admin@127.0.0.1:3306/bdvacunatorio'
     Bootstrap(app)
     db.init_app(app)
     return app

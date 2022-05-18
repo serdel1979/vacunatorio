55048efbde052679591f5e9669f153ec7e14099b
Sergio De Luca <sdlbsso@gmail.com>
2022-05-18 00:44:11 -0300

    arreglo del input fecha y la validación en el registro

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -1,4 +1,7 @@
+import datetime
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
+from pymysql import Date
 from wtforms import StringField, PasswordField, BooleanField, DateField, SelectField, Form, IntegerField
 from wtforms.validators import InputRequired, Email, Length
 
@@ -12,15 +15,34 @@ class LoginForm(FlaskForm):
 class RecuperarClave(FlaskForm):
     email = StringField('Ingrese su email',validators=[InputRequired(), Email(message='Email inválido'), Length(max=100)])
 
+
+
+class NullableDateField(DateField):
+    """Native WTForms DateField throws error for empty dates.
+    Let's fix this so that we could have DateField nullable."""
+    def process_formdata(self, valuelist):
+        if valuelist:
+            date_str = ' '.join(valuelist).strip()
+            if date_str == '':
+                self.data = None
+                return
+            try:
+                self.data = datetime.datetime.strptime(date_str, self.format).date()
+            except ValueError:
+                self.data = None
+                raise ValueError(self.gettext('Not a valid date value'))
+
+
+
 class RegistroForm(FlaskForm):
     nombre = StringField('Nombre',validators=[InputRequired(),Length(min=4,max=100)])
     apellido = StringField('Apellido',validators=[InputRequired(),Length(min=4,max=100)])
     telefono = IntegerField('Teléfono',validators=[InputRequired()])
-    nacimiento = DateField('Fecha de nacimiento')
+    nacimiento = DateField('Fecha de nacimiento',validators=[InputRequired('Fecha de nacimiento inválida')])
     primera_dosis = BooleanField('Tengo la primera dosis de covid')
-    fecha_primera_dosis = DateField('')
+    fecha_primera_dosis = NullableDateField('')
     segunda_covid = BooleanField('Tengo dos dosis de covid')
-    fecha_ultima_gripe = DateField('')
+    fecha_ultima_gripe = NullableDateField('')
     fiebre_amarilla = BooleanField('Estoy vacunado para la fiebre amarilla')
     paciente_riesgo = BooleanField('Soy paciente de riesgo')
     password = PasswordField('Escriba una contraseña',validators=[InputRequired(),Length(min=4,max=100)])
@@ -37,6 +59,12 @@ class RegistroForm(FlaskForm):
                 return True
         else:
             return True
+    
+    def validate_date(form, field):
+        return None
+    
+  
+
 
 class EnfermeroForm(FlaskForm):
     nombre = StringField('Nombre',validators=[InputRequired(),Length(min=4,max=100)])
